844aefb2543e649ed789955cdafae012e0657f04
ammarsjw <ammarsjw@gmail.com>
2023-09-06 14:45:39 +0500

    add foundry environment

---
+++ test/Zars.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+import { Zars } from "../contracts/Zars.sol";
+
+contract CounterTest is Test {
+    Zars zars;
+
+    function setUp() public {
+        // zars = new Zars();
+    }
+}
