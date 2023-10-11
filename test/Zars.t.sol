930b9b6d67242b8367f8f1445e18a2f8a8d7aede
ammarsjw <ammarsjw@gmail.com>
2023-10-11 16:03:29 +0500

    cc

--- test/Zars.t.sol
+++ test/Zars.t.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.20;
 
 import "forge-std/Test.sol";
 
-import { Zars } from "../contracts/Zars.sol";
+import { Zars } from "contracts/Zars.sol";
 
 contract CounterTest is Test {
     Zars zars;
