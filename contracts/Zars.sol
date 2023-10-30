d66cdb1520bf81acb90f76907af0e69e31d5dd68
ammarsjw <ammarsjw@gmail.com>
2023-10-30 12:30:39 +0500

    change title

--- contracts/Zars.sol
+++ contracts/Zars.sol
@@ -10,7 +10,7 @@ import { ERC20 } from "./utils/ERC20.sol";
 import { Ownable } from "./utils/Ownable.sol";
 
 /**
- * @title Zars token.
+ * @title Zars.
  * @author Zars team.
  * @notice Primary token contract.
  */
